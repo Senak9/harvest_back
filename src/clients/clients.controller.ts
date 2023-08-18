5d45a2e80c5d910a3951dfb4bf11f62968b07017
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-08-18 11:15:01 -0300

    feat: list all clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ClientsService } from './clients.service';
 
 @Controller('clients')
-export class ClientsController {}
+export class ClientsController {
+  constructor(private clientsSevice: ClientsService) {}
+  @Get()
+  findAll() {
+    return this.clientsSevice.findAll();
+  }
+}
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Client } from './client.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+  constructor(
+    @InjectRepository(Client) private clientsRepository: Repository<Client>,
+  ) {}
+
+  findAll() {
+    return this.clientsRepository.find();
+  }
+}
